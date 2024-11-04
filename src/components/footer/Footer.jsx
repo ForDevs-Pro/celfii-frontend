25770d79e92cb0e83003c0a62b6813a77266fe5e
Mika <test@Tests-MacBook-Pro.local>
2024-11-04 13:50:04 -0300

    feat(footer): conditionally render questions section based on path

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,22 +2,27 @@ import { socialMediaIcons } from "./menu";
 
 import SocialIcons from "./SocialIcons";
 import ItemsContainer from "./ItemsContainer";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
+  const { pathname } = useLocation();
+  
   return (
     <footer className="bg-black mt-60">
-      <div className="p-16 text-center bg-gray-50 md:py-32 md:px-40">
-        <p className="font-medium">Estamos aquí para ti</p>
-        <h3 className="my-5 text-4xl font-semibold font-poppins">¿Tienes Preguntas?</h3>
-        <p className="px-40 font-medium">
-          En Cel-Fii, valoramos tu experiencia y queremos asegurarnos de que tengas toda la
-          información que necesitas. Si tienes alguna pregunta sobre nuestros productos, servicios o
-          cualquier otro tema, no dudes en contactarnos a través de WhatsApp. Nuestro equipo está
-          disponible para ayudarte y responder a tus inquietudes de manera rápida y eficiente.
-          ¡Escríbenos y hablemos!
-        </p>
-        <button className="mt-8 primary-btn">Abrir Chat</button>
-      </div>
+      {(!pathname.startsWith("/product") && !pathname.startsWith("/admin")) && (
+        <div className="p-16 text-center bg-gray-50 md:py-32 md:px-40">
+          <p className="font-medium">Estamos aquí para ti</p>
+          <h3 className="my-5 text-4xl font-semibold font-poppins">¿Tienes Preguntas?</h3>
+          <p className="px-40 font-medium">
+            En Cel-Fii, valoramos tu experiencia y queremos asegurarnos de que tengas toda la
+            información que necesitas. Si tienes alguna pregunta sobre nuestros productos, servicios
+            o cualquier otro tema, no dudes en contactarnos a través de WhatsApp. Nuestro equipo
+            está disponible para ayudarte y responder a tus inquietudes de manera rápida y
+            eficiente. ¡Escríbenos y hablemos!
+          </p>
+          <button className="mt-8 primary-btn">Abrir Chat</button>
+        </div>
+      )}
       <ItemsContainer />
       <div className="container flex items-center justify-between pt-2 pb-8 text-sm text-center text-gray-400">
         <span>
